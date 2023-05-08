324d65d87b015899acad81f010f28dc6dadefd71
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-08 09:59:12 +0100

    adding second route

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,7 @@ const { getAllTasks } = require('../controllers/tasks.js');
 const Tasksrouter = express.Router();
 
 Tasksrouter.route('/').get(getAllTasks).post()
+Tasksrouter.route('/:id').get().patch().delete()
 
 
 
