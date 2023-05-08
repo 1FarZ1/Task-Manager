a83dc71eec2f51f777e10b18474070a5aecdc095
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-08 09:40:22 +0100

    the mvc pattern

--- public/browser-app.js
+++ public/browser-app.js
@@ -3,6 +3,8 @@ const loadingDOM = document.querySelector('.loading-text')
 const formDOM = document.querySelector('.task-form')
 const taskInputDOM = document.querySelector('.task-input')
 const formAlertDOM = document.querySelector('.form-alert')
+
+
 // Load tasks from /api/tasks
 const showTasks = async () => {
   loadingDOM.style.visibility = 'visible'
